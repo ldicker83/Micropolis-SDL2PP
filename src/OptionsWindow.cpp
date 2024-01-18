e917c141ed40cfaa7b78146df539902562f5a175
Leeor Dicker <leeor_net@yahoo.com>
2024-01-18 00:20:16 -0500

    Add stub class for OptionsWindow

---
+++ src/OptionsWindow.cpp
@@ -0,0 +1,17 @@
+#include "OptionsWindow.h"
+
+
+OptionsWindow::OptionsWindow(SDL_Renderer* renderer):
+	mRenderer{ renderer }
+{
+}
+
+
+void OptionsWindow::draw()
+{
+}
+
+
+void OptionsWindow::onMouseDown(const Point<int>&)
+{
+}
